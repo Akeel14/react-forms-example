d817d15939040cacef89485b1787734f70ab7111
Akeel Lashley <akeellashley@gmail.com>
2023-10-09 02:26:52 -0400

    adding formik and yup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,116 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Form, Container, Button } from 'react-bootstrap';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      gitHubUsername: '',
+      blogUrl: '',
+    },
+    validationSchema: Yup.object({
+      name: Yup.string()
+        .matches(
+          /^[a-zA-Z0-9 ]+$/,
+          'Name must be letters, dashes, numbers, or spaces'
+        )
+        .required('Required'),
+      email: Yup.string().email('Invalid Email Address').required('Required'),
+      gitHubUsername: Yup.string()
+        .min(5, 'Must be at least 5 characters')
+        .max(15, 'Must be at most 15 characters')
+        .required(),
+      blogUrl: Yup.string().url('Must be a valid URL'),
+    }),
+    onSubmit: (values) => {
+      // Here, you handle what you want to do with the form data when the form is submitted.
+      // For instance, sending the data to a server.
+      console.log(values);
+    },
+  });
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container fluid className="signup-form">
+      <Form method="post" action="#" onSubmit={formik.handleSubmit}>
+        <Form.Group>
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Enter your name"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.name}
+          />
+          {formik.touched.name && formik.errors.name ? (
+            <Form.Text className="text-muted">{formik.errors.name}</Form.Text>
+          ) : null}
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            id="email"
+            name="email"
+            type="email"
+            placeholder="Enter your email address"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.email}
+          />
+          {formik.touched.email && formik.errors.email ? (
+            <Form.Text className="text-muted">{formik.errors.email}</Form.Text>
+          ) : null}
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>GitHub Username</Form.Label>
+          <Form.Control
+            id="gitHubUsername"
+            name="gitHubUsername"
+            type="text"
+            placeholder="Enter your GitHub Username"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.gitHubUsername}
+          />
+          {formik.touched.gitHubUsername && formik.errors.gitHubUsername ? (
+            <Form.Text className="text-muted">
+              {formik.errors.gitHubUsername}
+            </Form.Text>
+          ) : null}
+        </Form.Group>
+
+        <Form.Group>
+          <Form.Label>Blog URL</Form.Label>
+          <Form.Control
+            id="blogUrl"
+            name="blogUrl"
+            type="url"
+            placeholder="Enter your Blog URL"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.blogUrl}
+          />
+          {formik.touched.blogUrl && formik.errors.blogUrl ? (
+            <Form.Text className="text-muted">
+              {formik.errors.blogUrl}
+            </Form.Text>
+          ) : null}
+        </Form.Group>
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </Container>
+  );
 }
 
-export default App
+export default App;
